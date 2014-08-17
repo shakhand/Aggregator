e0e126020f049dc6885efea19800a798fc177c5b
shakhand <hurricane_200@163.com>
2014-08-17 08:48:25 +0800

    remve useless code

--- books/views.py
+++ books/views.py
@@ -15,19 +15,6 @@ def search(request):
     else:
         return render_to_response('books/search_form.html', {'infomation':'Please submit a search term.'})
         
-def contact(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            # send_mail(  cd['subject'],
-                        # cd['message'],
-                        # cd.get('email', 'noreply@example.com'),
-                        # ['hurricane_200@163.com'],)
-            return HttpResponseRedirect('/contact/thanks')
-    else:
-        form = ContactForm()
-    return render_to_response('books/contact_form.html', {'form':form})
     
 def list_rss(request):
     entries = RSSEntry.objects.all()
